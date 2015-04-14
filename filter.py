55764ad6d67816c43407ca1e19c92b9b92632db8
Megawatto <test@test.com>
2015-04-15 02:00:02 +0400

    repeat git

--- filter.py
+++
@@ -1,9 +0,0 @@
-class Filter:
-    def appFilter_01(self, data):
-        return None
-
-    def appFilter_02(self, data):
-        return None
-
-    def appFilter_02(self, data):
-        return None
